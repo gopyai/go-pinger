a8c11770a5cf70e2459d087bab3a46f6e17010b3
gopyai <stefarf@me.com>
2017-10-30 22:16:03 +0700

    Add pinger library and its testing.

---
+++ pinger.go
@@ -0,0 +1,264 @@
+package pinger
+
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+)
+
+const (
+	protocolICMP = 1
+)
+
+type (
+	PingResult struct {
+		ReplyRatio     float64
+		LatencyAverage float64
+	}
+
+	bySeq struct {
+		ipIdx   int       // set when start echo
+		echoAt  time.Time //
+		latency float64   // updated at each reply
+	}
+
+	byIP struct {
+		times      int     // updated at each reply
+		latencySum float64 //
+	}
+)
+
+var (
+	ErrParseReply   = errors.New("error: Parse ICMP message expect ipv4.ICMPTypeEchoReply")
+	ErrDisconnected = errors.New("error: No active connection")
+	ErrSendEcho     = errors.New("error: Failed to send ICMP echo message")
+
+	conn struct {
+		sync.Mutex
+		*icmp.PacketConn
+		onReply map[int]func(seq int) // Map seq to callback function
+		seq     int
+	}
+
+	readBuffer = make([]byte, 1500)
+)
+
+func Start() error {
+	syncConnDestroy()
+	return syncConnCreate()
+}
+
+func Stop() {
+	syncConnDestroy()
+}
+
+func bgReply() error {
+	//if err := conn.SetReadDeadline(time.Now().Add(time.Second)); err != nil {
+	//	stop()
+	//	return err
+	//}
+
+	for {
+		seq, err := syncReplyRead()
+		if err != nil {
+			return err
+		}
+		syncReplyCall(seq)
+	}
+}
+
+func Ping(pingList []net.IP, times int, durBetween, durLast time.Duration) ([]*PingResult, error) {
+	rst := make([]*PingResult, len(pingList))
+	rstByIP := make([]*byIP, len(pingList))
+	rstBySeq := make(map[int]*bySeq)
+
+	for ipIdx := 0; ipIdx < len(pingList); ipIdx++ {
+		rst[ipIdx] = new(PingResult)
+		rstByIP[ipIdx] = new(byIP)
+	}
+
+	done := make(chan bool, 2)
+
+	for loop := 0; loop < times; loop++ {
+		for ipIdx, ip := range pingList {
+			err := syncPingEcho(ip,
+				func(seq int) { // On echo
+					rstBySeq[seq] = &bySeq{ipIdx: ipIdx, echoAt: time.Now()}
+				},
+				func(seq int) { // On reply
+					rs, ok := rstBySeq[seq]
+					if !ok {
+						return
+					}
+					rs.latency = time.Since(rs.echoAt).Seconds()
+
+					rip := rstByIP[rs.ipIdx]
+					rip.times++
+					rip.latencySum += rs.latency
+
+					delete(rstBySeq, seq)
+					if len(rstBySeq) == 0 {
+						done <- true
+					}
+				})
+			if err != nil {
+				return nil, err
+			}
+		}
+		time.Sleep(durBetween)
+	}
+
+	go func() {
+		time.Sleep(durLast)
+		syncPingClearSeq(rstBySeq)
+		done <- true
+	}()
+
+	<-done
+	return syncPingSummarize(rst, rstByIP, times)
+}
+
+func syncConnCreate() error {
+	conn.Lock()
+	defer conn.Unlock()
+	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		return err
+	}
+
+	conn.PacketConn = c
+	conn.onReply = make(map[int]func(int))
+	go bgReply()
+
+	return nil
+}
+
+func syncConnDestroy() {
+	conn.Lock()
+	defer conn.Unlock()
+	if conn.PacketConn == nil {
+		return // Already disconnected
+	}
+	conn.Close()
+
+	conn.PacketConn = nil
+	conn.onReply = nil
+}
+
+func syncReplyRead() (seq int, err error) {
+	conn.Lock()
+	c := conn.PacketConn
+	if c == nil {
+		conn.Unlock()
+		return 0, ErrDisconnected
+	}
+	conn.Unlock()
+
+	n, peer, err := c.ReadFrom(readBuffer)
+	if err != nil {
+		//switch err.(type) {
+		//case *net.OpError:
+		//	// Timeout error may happen
+		//	if strings.Contains(err.Error(), "i/o timeout") {
+		//		return ErrReadTimeout
+		//	}
+		//default:
+		//}
+		return 0, err
+	}
+
+	msg, err := icmp.ParseMessage(protocolICMP, readBuffer[:n])
+	if err != nil {
+		return 0, err
+	}
+
+	switch msg.Type {
+	case ipv4.ICMPTypeEchoReply:
+	default:
+		fmt.Println("error: Got", msg, "from", peer)
+		return 0, ErrParseReply
+	}
+
+	b, err := msg.Body.Marshal(protocolICMP)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(binary.BigEndian.Uint16(b[2:4])), nil
+}
+
+func syncReplyCall(seq int) {
+	conn.Lock()
+	defer conn.Unlock()
+	if onReply, ok := conn.onReply[seq]; ok {
+		onReply(seq)
+		delete(conn.onReply, seq)
+	}
+}
+
+func syncPingEcho(ip net.IP, onEcho, onReply func(seq int)) error {
+	conn.Lock()
+	defer conn.Unlock()
+
+	onEcho(conn.seq)
+	conn.onReply[conn.seq] = onReply
+
+	msg := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID:   os.Getpid() & 0xffff, Seq: conn.seq,
+			Data: []byte("HELLO-R-U-THERE"),
+		},
+	}
+	conn.seq++
+
+	b, err := msg.Marshal(nil)
+	if err != nil {
+		return err
+	}
+
+	n, err := conn.WriteTo(b, &net.IPAddr{IP: ip})
+	if err != nil {
+		return err
+	}
+	if n != len(b) {
+		return ErrSendEcho
+	}
+
+	return nil
+}
+
+func syncPingClearSeq(rstBySeq map[int]*bySeq) {
+	conn.Lock()
+	defer conn.Unlock()
+	for seq := range rstBySeq {
+		delete(rstBySeq, seq)
+	}
+}
+
+func syncPingSummarize(rst []*PingResult, rstByIP []*byIP, times int) ([]*PingResult, error) {
+	conn.Lock()
+	defer conn.Unlock()
+	for ipIdx, r := range rst {
+		rip := rstByIP[ipIdx]
+		if rip.times == 0 {
+			continue
+		}
+		r.ReplyRatio = float64(rip.times) / float64(times)
+		r.LatencyAverage = rip.latencySum / float64(rip.times)
+	}
+	return rst, nil
+}
+
+//func isErr(err error) {
+//	if err != nil {
+//		panic(err)
+//	}
+//}
---
+++ pinger_test.go
@@ -0,0 +1,37 @@
+package pinger_test
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/gopyai/pinger"
+)
+
+func Example() {
+	// NOTICE: This code requires super user privilege to run
+	isErr(pinger.Start())
+	isErr(pinger.Start()) // restart / reconnect
+	pinger.Stop()
+
+	isErr(pinger.Start())
+	defer pinger.Stop()
+
+	pingList := []net.IP{
+		net.IPv4(172, 217, 24, 4),
+		net.IPv4(31, 13, 78, 35),
+		net.IPv4(127, 0, 0, 1),
+	}
+	rst, err := pinger.Ping(pingList, 10, time.Millisecond, time.Second*5)
+	isErr(err)
+
+	for idx, r := range rst {
+		fmt.Printf("ping %v success %.1f%% with latency %.2f ms\n", pingList[idx], r.ReplyRatio*100, r.LatencyAverage*1000)
+	}
+}
+
+func isErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
